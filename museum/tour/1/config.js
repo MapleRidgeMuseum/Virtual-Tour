531ee98d7595a6fb09dc971ee35c34cdd1053cd5
yowasap <darrenrobinragoonath@gmail.com>
2020-11-25 12:50:29 -0800

    first commit

---
+++ museum/tour/1/config.js
@@ -0,0 +1,11 @@
+/*
+* Theasys
+* Generated on: Wednesday 25th of November 2020 05:34:06 PM
+* Config file: You can change the path to match your server path.
+*/
+
+var vars = {
+    file : 'index.html',
+    path : 'http://localhost',//path to the index.html
+    theme : 'theasys',
+};
---
+++ museum/tour/2/config.js
@@ -0,0 +1,11 @@
+/*
+* Theasys
+* Generated on: Wednesday 25th of November 2020 05:36:10 PM
+* Config file: You can change the path to match your server path.
+*/
+
+var vars = {
+    file : 'index.html',
+    path : 'http://localhost',//path to the index.html
+    theme : 'theasys',
+};
